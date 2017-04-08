244c48c85060aa2ac59908ac622ac3ced4a6c8e0
Aurore Gaucher <gaucher.aurore@laposte.net>
2017-04-08 14:38:35 +0200

    security controller

---
+++ src/BJ/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aigie
+ * Date: 08/04/2017
+ * Time: 14:32
+ */
+
+namespace BJ\UserBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction()
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        //TODO définir vue login
+        return $this->render('', array(
+            'last_username' => $lastUsername,
+            'error'         => $error
+        ));
+    }
+}
